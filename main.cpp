df29a1d832758753b20bb1fdb840d64c4beea0d9
klee133 <klee133@fordham.edu>
2022-12-09 09:49:39 -0500

    RGB transitions left->right, black to white transitions top->down

--- main.cpp
+++ main.cpp
@@ -10,19 +10,38 @@ vec3 color(const ray& r) {
     vec3 unit_direction = unit_vector(r.direction());
     float t = 0.5 * (unit_direction.y() + 1.0);
     float h = 0.5 * (unit_direction.x() + 1.0);
-    vec3 vert = (1.0-t)*vec3(1.0, 1.0,1.0) + t*vec3(0.0,1.0,0.0);
     vec3 hori = vec3(0.0, 0.0, 0.0);
-    if(h < 0.5){
-        hori = (0.5-h)*2*vec3(1.0, 0.0,0.0) + h*2*vec3(1.0,1.0,1.0)
-                - (0.5-h)*2*t*vec3(1.0, 0.0,0.0);
-        vert -= t*(1.0-h*2)*vec3(0.0,1.0,0.0);
-    }else{
-        hori = (1.0-h)*2*vec3(1.0, 1.0,1.0) + (h-0.5)*2*vec3(0.0,0.0,1.0)
-                - (h-0.5)*2*t*vec3(0.0,0.0,1.0);
-        vert -= t*(h-0.5)*2*vec3(0.0,1.0,0.0);
+    int first = 0;
+    int second = 2;
+
+    // Assigns values based on the x unit direction
+    while(h > 0.166666){
+        h -= 0.166666;
+        first = (first+1)%6;
+        second = (second+1)%6;
     }
 
-    return (vert + hori) / 2;
+    // Transitions: R->RG->G->GB->B->BR->R
+    // If even, one value starts at 1.0 and the next increases from 0.0
+    if(first % 2 == 0){
+        hori.e[first/2] = 1;
+        hori.e[second/2] = h/0.166666;
+    // If odd, two values start at 1.0 and the first one decreases to 0.0
+    }else{
+        hori.e[first/2] = (0.166666-h)/0.166666;
+        hori.e[second/2] = 1;
+    }
+
+    // Value based on the y unit direction
+    // Results in colors transitioning from black to full color to white, vertically
+    vec3 result;
+    if(t > 0.5){
+        result = hori*(1.0-t)*2;
+    }else{
+        result = (vec3(1.0,1.0,1.0)-hori)*(0.5-t)*2 + hori;
+    }
+
+    return result;
 }
 
 int main()
@@ -57,26 +76,3 @@ int main()
 
     return 0;
 }
-
-
-
-/*
-    int nx = 200;
-    int ny = 100;
-
-    ofstream myfile;
-    myfile.open("/Users/admin/RayTracing_Weekend/ColorTest.ppm");
-    myfile << "P3\n" << nx << " " << ny << "\n255\n";
-
-    for(int j = ny-1; j >= 0; j--){
-        for(int i = 0; i < nx; i++){
-            vec3 col(float(i) / float(nx), float(j) / float(ny), 0.2);
-            int ir = int(255.99*col[0]);
-            int ig = int(255.99*col[1]);
-            int ib = int(255.99*col[2]);
-
-            myfile << ir << " " << ig << " " << ib << "\n";
-        }
-    }
-
-*/
