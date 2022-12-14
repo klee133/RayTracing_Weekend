5717aa10c8ffeda3073b473580514b2de9a545c3
klee133 <klee133@fordham.edu>
2022-12-14 17:10:22 -0500

    Added rotation and move functionality to camera, fixed cross() in vec3.h

--- material.h
+++ material.h
@@ -58,8 +58,9 @@ public:
     vec3 albedo;
     float fuzz;
 
-    metal(const vec3& a) : albedo(a) {fuzz = 0;}
+    metal(const vec3& a) : albedo(a) {fuzz = 0.25;}
     metal(const vec3& a, float f) : albedo(a) {if(f<1) fuzz = f; else fuzz = 1;}
+
     virtual bool scatter(const ray& r_in, const hit_record& rec,
                          vec3& attenuation, ray& scattered) const {
         vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
@@ -67,6 +68,7 @@ public:
         attenuation = albedo;
         return(dot(scattered.direction(), rec.normal) > 0);
     }
+
 };
 
 class dielectric : public material {
